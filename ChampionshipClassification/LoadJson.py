1548014eec243dd682f3de9c470952a41f6e73d4
Gianluca Gorlani <piccolorisparmiatore@gmail.com>
2019-10-12 08:53:01 +0200

    "correzione di una parola ;-)"

--- ChampionshipClassification/LoadJson.py
+++ ChampionshipClassification/LoadJson.py
@@ -4,6 +4,6 @@ def loadJson():
     b = {}
     with open('classification.json', 'r') as rjs:
         arrey=json.load(rjs)
-        for kays, values in arrey.items():
-            b.update({kays: values})
+        for keys, values in arrey.items(): # corretto "kays" (GG)
+            b.update({keys: values})
     return b
